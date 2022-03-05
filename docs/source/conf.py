31eebea303655aa8ac9565cf5f6de7eb2868f7e9
sandy1618 <sandeepnayak1.618@gmail.com>
2022-03-05 16:40:23 +0530

    console highlighting , upgrade ipython

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,7 +38,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['_build', '**.ipynb_checkpoints']
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -64,6 +64,7 @@ extensions = [
     'nbsphinx',
     'sphinx.ext.mathjax',
     'sphinx.ext.githubpages',
+    'IPython.sphinxext.ipython_console_highlighting',
 ]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
